3a22f4762bc260f9a83293c87235ad091a0c1155
Leyz1 <leyz1@proton.me>
2026-05-19 16:59:17 +0200

    Migration from HTTP server to HTTPS

--- app/src/main/java/com/example/mindmath/api/RetrofitClient.java
+++ app/src/main/java/com/example/mindmath/api/RetrofitClient.java
@@ -6,7 +6,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private static final String BASE_URL = "http://2.27.40.143:8080/";
+    private static final String BASE_URL = "https://sub.lazyvepene.mooo.com/";
     private static Retrofit retrofit;
     private RetrofitClient() {
     }
